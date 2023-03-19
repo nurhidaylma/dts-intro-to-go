f9171adf85ee3c1e002697712d045c96e94769f1
ilma <ilma.nur@bluebirdgroup.com>
2023-03-19 21:53:15 +0700

    modify sesi-2

--- sesi-2/main.go
+++ sesi-2/main.go
@@ -5,9 +5,6 @@ import (
 )
 
 func main() {
-	// bs_UTF16LE, _, _ := transform.Bytes(unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM).NewEncoder(), []byte("測試"))
-
-	// text := []string{"C", "A", "Ш", "A", "P", "B", "O"}
 	textMap := make(map[int]string)
 	textMap[0] = "C"
 	textMap[2] = "A"
@@ -28,7 +25,7 @@ func main() {
 
 		if j == 5 {
 			for i, v := range textMap {
-				fmt.Printf("character %+q '%s' starts at byte position %d\n", v, v, i)
+				fmt.Printf("character %U '%s' starts at byte position %d\n", i, v, i)
 			}
 			continue
 		}
